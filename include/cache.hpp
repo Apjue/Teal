bfb69d38ced1e9fe5e905788341dfd82e836b01e
S6066 <samy6066@yahoo.fr>
2016-09-04 17:59:27 +0200

    Deleted old cache

--- include/cache.hpp
+++ include/cache.hpp
@@ -61,43 +61,6 @@ protected:
 /// \note ProduceType must have a create() function
 ///
 
-// template<class Key, class T, class ProduceType = DefaultProducer<T>>
-// class CreateCache
-// {
-//     using ManagerType = decltype(ProduceType::create());
-//     using InternalCache = typename std::unordered_map<Key, ManagerType>;
-// 
-// public:
-//     CreateCache() = default;
-//     ~CreateCache() = default;
-// 
-//     template<class... Args>
-//     ManagerType get(const Key& k, Args&&... args)
-//     {
-//         auto it = m_objects.find(k);
-// 
-//         if (it == m_objects.end())
-//             return add(k, std::forward<Args>(args)...)->second;
-//         else
-//             return it->second;
-//     }
-// 
-//     template<class... Args>
-//     ManagerType add(const Key& k, Args&&... args)
-//     {
-//         return add_(k, std::forward<Args>(args)...)->second;
-//     }
-// 
-// private:
-//     InternalCache m_objects;
-// 
-//     template<class... Args>
-//     typename InternalCache::iterator add_(const Key& k, Args&&... args)
-//     {
-//         return m_objects.emplace(k, ProduceType::create(std::forward<Args>(args)...)).first;
-//     }
-// };
-
 template<class Key, class T, class ProduceType = DefaultProducer<T>>
 class CreateCache : private Cache<Key, T, ProduceType>
 {
