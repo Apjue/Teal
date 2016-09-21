e5ff1b2f30a7984d4e2ce0f945912d81134ec57b
S6066 <samy6066@yahoo.fr>
2016-09-21 13:38:34 +0200

    Changed comment

--- include/def/gamedef.hpp
+++ include/def/gamedef.hpp
@@ -13,15 +13,15 @@
 namespace Def
 {
 
-constexpr unsigned MAPX           { 15u }; //For arrays.
+constexpr unsigned MAPX           { 15u }; // For arrays.
 constexpr unsigned MAPY           { 8u };
 
 constexpr unsigned TILEARRAYSIZE  { MAPX * MAPY };
 
-constexpr unsigned LMAPX          { 14u }; //For logic
+constexpr unsigned LMAPX          { 14u }; // For logic
 constexpr unsigned LMAPY          { 14u };
 
-constexpr unsigned TILEXSIZE      { 64u }; // Used for logic
+constexpr unsigned TILEXSIZE      { 64u }; // For logic
 constexpr unsigned TILEYSIZE      { 32u };
 
 constexpr unsigned SQUARETILESIZE { 64u }; // Used for graphics TileMap texture coords (x and y)
@@ -33,9 +33,9 @@ constexpr unsigned MAXPOSINTILE   { 4u };
 constexpr unsigned MAXGXPOSINTILE { TILEGXSIZE / MAXPOSINTILE };
 constexpr unsigned MAXGYPOSINTILE { TILEGYSIZE / MAXPOSINTILE };
 
-constexpr float    MAXFPS         { 10 }; // Updates per second
+constexpr  float   MAXFPS         { 10 }; // Updates per second
 
-constexpr int      MAXDIR         { 4 }; // Max directions: Up, down, left, right
+constexpr   int    MAXDIR         { 4 }; // Max directions: Up, down, left, right
 
 }
 
