87cba61dafdc5eac9ac5104866cf2face94cff71
S6066 <samy6066@yahoo.fr>
2016-10-23 19:15:47 +0200

    Removed useless util.cpp file

--- src/util/util.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright (C) 2016 Samy Bensaid
-// This file is part of the Teal game.
-// For conditions of distribution and use, see copyright notice in LICENSE
-
-#include "util/util.hpp"
-
-//...
