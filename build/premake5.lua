7beb44840a9c2b110321c96a554d49bc64a12202
S6066 <samy6066@yahoo.fr>
2016-10-26 10:19:10 +0200

    Added premake script

---
+++ build/premake5.lua
@@ -0,0 +1,46 @@
+workspace "Teal"
+    configurations { "Debug", "Release" }
+    platforms { "x86", "x64" }
+
+    filter "platforms:x86"
+        architecture "x32"
+
+    filter "platforms:x64"
+        architecture "x64"
+
+    filter {}
+
+project "TealDemo"
+    kind "ConsoleApp"
+    language "C++"
+    targetdir "%{cfg.buildcfg}/%{cfg.platform}/"
+
+    files
+    {
+        "../include/**.hpp",
+        "../include/**.inl",
+        "../src/**.cpp"
+    }
+
+    local compiler = _ACTION
+
+    if (_ACTION == nil) then
+        compiler = "compiler"
+    end
+
+    libdirs { "../extlibs/lib/" .. compiler ..  "/%{cfg.platform}/micropather/", "../extlibs/lib/" .. compiler ..  "/%{cfg.platform}/nazara/" }
+    includedirs { "../extlibs/include/micropather/", "../extlibs/include/nazara/", "../include/" }
+    flags { "C++14" }
+
+    filter "action:vs*"
+        defines { "_CRT_SECURE_NO_WARNINGS", "_SCL_SECURE_NO_WARNINGS" } -- Used to suppress some errors
+
+    filter "configurations:Debug"
+        defines { "TEAL_DEBUG", "NAZARA_DEBUG" }
+        links { "NazaraCore-d", "NazaraGraphics-d", "NazaraRenderer-d", "NazaraUtility-d", "NazaraSDK-d", "NazaraLua-d", "micropather-d" }
+        flags { "symbols" }
+
+    filter "configurations:Release"
+        defines { "NDEBUG" }
+        links { "NazaraCore", "NazaraGraphics", "NazaraRenderer", "NazaraUtility", "NazaraSDK", "NazaraLua", "micropather" }
+        optimize "On"
