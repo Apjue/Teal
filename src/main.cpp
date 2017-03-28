c04059b1315c35ea6bcfde1e10e09049211363cb
S6066 <kiwimeteor6066@gmail.com>
2017-03-28 18:05:23 +0200

    OpenAL workaround

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-// Copyright (C) 2016 Samy Bensaid
+﻿// Copyright (C) 2016 Samy Bensaid
 // This file is part of the Teal game.
 // For conditions of distribution and use, see copyright notice in LICENSE
 
@@ -8,12 +8,13 @@
 
 int main()
 {
+    Nz::Initializer<Ndk::Sdk> sdk;
     Ndk::Application app;
 
     Game game { app, { Def::WINXSIZE, Def::WINYSIZE },
                      { Def::MAPXVIEWPORT, Def::MAPYVIEWPORT }, "TealDemo - Prototype" };
 
-//     app.EnableFPSCounter(true);
+    // app.EnableFPSCounter(true);
     
     while (app.Run())
     {
