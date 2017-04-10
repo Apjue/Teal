f7a3c4196f834c74821b8820099f05de25abacdd
S6066 <samy6066@yahoo.fr>
2017-04-10 21:34:28 +0200

    No functions for components.

--- include/components/common/lifecomponent.hpp
+++ include/components/common/lifecomponent.hpp
@@ -9,28 +9,13 @@
 
 #include <NDK/Component.hpp>
 
-class LifeComponent : public Ndk::Component<LifeComponent>
+struct LifeComponent : public Ndk::Component<LifeComponent>
 {
-public:
     LifeComponent(unsigned maxhp) : m_hp { maxhp }, m_maxhp { maxhp } {}
     ~LifeComponent() = default;
 
-    inline bool alive() const noexcept;
-    inline unsigned HP() const noexcept;
-    inline void setHP(unsigned hp);
-
-    inline unsigned maxHP() const noexcept;
-    inline void setMaxHP(unsigned maxHP);
-
-    static Ndk::ComponentIndex componentIndex;
-
-private:
     unsigned m_hp { 100 };
     unsigned m_maxhp { 100 };
-
-    inline void verifyInfos();
 };
-
-#include "lifecomponent.inl"
 
 #endif // LIFECOMPONENT_HPP
