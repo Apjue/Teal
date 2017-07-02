77466d3cbfa389c410e7c4afa063cf395a072dce
S6066 <kiwimeteor6066@gmail.com>
2017-07-02 16:19:53 +0200

    Added ctor

--- include/components/common/levelcomponent.hpp
+++ include/components/common/levelcomponent.hpp
@@ -1,4 +1,4 @@
-// Copyright (C) 2016 Samy Bensaid
+﻿// Copyright (C) 2016 Samy Bensaid
 // This file is part of the TealDemo project.
 // For conditions of distribution and use, see copyright notice in LICENSE
 
@@ -11,6 +11,8 @@
 
 struct LevelComponent : public Ndk::Component<LevelComponent>
 {
+    LevelComponent(unsigned lvl = 1) : level { lvl } {}
+
     unsigned level {};
 
     static Ndk::ComponentIndex componentIndex;
