4a3144b44672adc88b484bf952d069dfe4a4d8e9
S6066 <kiwimeteor6066@gmail.com>
2017-07-02 16:22:43 +0200

    Ternary is better

--- src/components/common/mapcomponent.cpp
+++ src/components/common/mapcomponent.cpp
@@ -102,14 +102,7 @@ bool MapInstance::update() // Thanks Lynix for this code
                 indexMapper.Set(index + 5, vertex + 0);
 
                 const TileData& tile = m_map->tile(XYToIndex(x, y)); /// \todo Invisible tile
-                unsigned tileNumber {};
-
-                if (m_fightMode)
-                    tileNumber = m_fightTilesetCore->get(tile.fightTextureId);
-
-                else
-                    tileNumber = m_tilesetCore->get(tile.textureId);
-
+                unsigned tileNumber = m_fightMode ? m_fightTilesetCore->get(tile.fightTextureId) : m_tilesetCore->get(tile.textureId);
 
                 float textureX = tileSize.x * tileNumber;
                 float tilesetSize = m_fightMode ? Def::FIGHTTILESETSIZE : Def::TILESETSIZE;
