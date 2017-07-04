8eeeea63463c18694021cb145e753bd597358751
S6066 <kiwimeteor6066@gmail.com>
2017-07-04 16:33:54 +0200

    spaces ftw

--- wdirs/data/script/items/excalibur.lua
+++ wdirs/data/script/items/excalibur.lua
@@ -1,19 +1,19 @@
 teal_item =
 {
-	name = "Excalibur",
-	desc = "Legendary Sword",
-	level = 1,
+    name = "Excalibur",
+    desc = "Legendary Sword",
+    level = 1,
 
-	components =
-	{
-		{
-			component = "AttackModifier",
-			0, 0, 150
-		},
+    components =
+    {
+        {
+            component = "AttackModifier",
+            0, 0, 150
+        },
 
-		{
-			component = "ResistanceModifier",
-			100, 100, 100, 0, 100
-		}
-	}
+        {
+            component = "ResistanceModifier",
+            100, 100, 100, 0, 100
+        }
+    }
 }
