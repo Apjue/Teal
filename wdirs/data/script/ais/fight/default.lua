5dc40b2df5905e25a4cb7c6156de17994dd5b62b
S6066 <kiwimeteor6066@gmail.com>
2017-10-03 19:16:01 +0200

    Changed types & name

--- wdirs/data/script/ais/fight/default.lua
+++ wdirs/data/script/ais/fight/default.lua
@@ -1,12 +1,13 @@
 -- Default Fight AI
 
 fight_ai_type = "default"
-fight_ai_name = "default"
+fight_ai_monstername = "*"
+fight_ai_name = "Default AI"
 
-function compute()
+function execute()
     local data = teal_fight_data
     local character = data.character
 
-    character:MoveAndAttack(character:ChooseTarget())
+    character:MoveAndAttack()
     character:TakeCover()
 end
