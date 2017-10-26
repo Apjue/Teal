03db61557f7b57bace074243ea198456236cbf57
S6066 <kiwimeteor6066@gmail.com>
2017-10-26 15:24:15 +0200

    Added try-catch block in main function

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-// Copyright (C) 2016 Samy Bensaid
+// Copyright (C) 2017 Samy Bensaid
 // This file is part of the Teal game.
 // For conditions of distribution and use, see copyright notice in LICENSE
 
@@ -6,19 +6,39 @@
 #include "def/uidef.hpp"
 #include "game.hpp"
 
+#ifndef TEAL_DEBUG
+#include <exception>
+#include <iostream>
+#endif
+
 int main()
 {
     Ndk::Application app;
 
-    Game game { app, { Def::WindowSizeX, Def::WindowSizeY },
-                     { Def::MapSizeX, Def::MapSizeY }, "TealDemo - Prototype" };
-
-    // app.EnableFPSCounter(true);
-    
-    while (app.Run())
+    #ifndef TEAL_DEBUG
+    try
     {
-        game.run();
+    #endif
+
+        Game game { app, { Def::WindowSizeX, Def::WindowSizeY },
+                            { Def::MapSizeX, Def::MapSizeY }, "TealDemo - Prototype" };
+
+        // app.EnableFPSCounter(true);
+
+        while (app.Run())
+        {
+            game.run();
+        }
+
+    #ifndef TEAL_DEBUG
     }
+
+    catch (const std::exception& e)
+    {
+        std::cerr << "\n ===============\n" << "A problem occurred!\nError message: ";
+        std::cerr << e.what() << "\n ===============\n" << std::endl;
+    }
+    #endif
 
     return 0;
 }
