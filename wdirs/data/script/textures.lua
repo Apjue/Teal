d8ab9c0da78d64eb0046a2eeea3c01aa7cdd7d35
S6066 <kiwimeteor6066@gmail.com>
2017-11-19 10:59:26 +0100

    Added some textures

--- wdirs/data/script/textures.lua
+++ wdirs/data/script/textures.lua
@@ -1,10 +1,19 @@
 teal_textures =
 {
-    { ":/game/money", "game/main/money.png" },
-    { ":/game/scheme", "game/main/scheme.png" },
-    { ":/game/teal", "game/main/teal.png" },
-    { ":/game/tileset", "game/main/tileset.png" },
+    { ":/game/money",         "game/main/money.png" },
+    { ":/game/scheme",        "game/main/scheme.png" },
+    { ":/game/teal",          "game/main/teal.png" },
+    { ":/game/tileset",       "game/main/tileset.png" },
     { ":/game/fight_tileset", "game/main/tileset_fight.png" },
     { ":/game/char/villager", "game/char/villager.png" },
-    { ":/game/unknown", "game/main/unknown.png" }
+    { ":/game/unknown",       "game/main/unknown.png" },
+
+    { ":/buttons/equip",         "buttons/bar/equip.png" },
+    { ":/buttons/equip_pressed", "buttons/bar/equip_pressed.png" },
+    { ":/buttons/inv",           "buttons/bar/inv.png" },
+    { ":/buttons/inv_pressed",   "buttons/bar/inv_pressed.png" },
+	
+    { ":/buttons/close", "buttons/ui/close.png" },
+    { ":/buttons/minus", "buttons/ui/minus.png" },
+    { ":/buttons/plus",  "buttons/ui/plus.png" }
 }
