1fefa7b2b7de1d86cdb584c9286ad25ad1650840
S6066 <kiwimeteor6066@gmail.com>
2018-01-01 00:08:20 +0100

    ++year;

--- include/data/elementdata.hpp
+++ include/data/elementdata.hpp
@@ -1,4 +1,4 @@
-// Copyright (C) 2017 Samy Bensaid
+// Copyright (C) 2018 Samy Bensaid
 // This file is part of the  project.
 // For conditions of distribution and use, see copyright notice in LICENSE
 
