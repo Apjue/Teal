8abc26b195eb4d1650f6e8f5a5211e9cfc9998a6
S6066 <kiwimeteor6066@gmail.com>
2018-06-15 17:22:02 +0200

    Move CharacterData's LuaImpl implementations to .inl

--- include/data/characterdata.hpp
+++ include/data/characterdata.hpp
@@ -116,105 +116,16 @@ struct CharacterData
 };
 
 #include <Nazara/Lua/LuaState.hpp>
-#include <NDK/LuaAPI.hpp>
-#include "util/luaparser.hpp"
 
 namespace Nz
 {
 
-inline unsigned int LuaImplQueryArg(const LuaState& state, int index, CharacterData::RandomMovement* random, TypeTag<CharacterData::RandomMovement>)
-{
-    if (state.GetType(index) != Nz::LuaType_Table)
-        return 1;
-
-    random->randomMovement = true;
-    random->movementInterval = state.CheckField<float>("movement_interval", index);
-    random->range = state.CheckField<unsigned>("range", index);
-
-    return 1;
-}
-
-inline unsigned int LuaImplQueryArg(const LuaState& state, int index, CharacterData::Fight* fight, TypeTag<CharacterData::Fight>)
-{
-    if (state.GetType(index) != Nz::LuaType_Table)
-        return 1;
-
-    fight->fight = true;
-    fight->autoAttack = state.CheckField<bool>("autoAttack", false, index);
-    fight->movementPoints = state.CheckField<unsigned>("movementPoints", 3, index);
-    fight->actionPoints = state.CheckField<unsigned>("actionPoints", 6, index);
-
-    return 1;
-}
-
-
-inline unsigned int LuaImplQueryArg(const LuaState& state, int index, CharacterData* data, TypeTag<CharacterData>)
-{
-    state.CheckType(index, Nz::LuaType_Table);
-
-    data->maxHp = state.CheckField<unsigned>("maxhealth", 100u, index);
-    data->orientation = stringToOrientation(state.CheckField<Nz::String>("orientation", "downleft", index));
-    data->blockTile = state.CheckField<bool>("blocktile", false, index);
-    data->name = state.CheckField<Nz::String>("name", "Unnamed", index);
-    data->description = state.CheckField<Nz::String>("description", "Empty", index);
-    data->level = state.CheckField<unsigned>("level", 1u, index);
-    data->offset = state.CheckField<Nz::Vector2f>("offset", Nz::Vector2f {}, index);
-
-
-    LuaArguments animArgs;
-
-    if (state.GetField("animations") == Nz::LuaType_Table)
-        parseLua(state, animArgs);
-
-    {
-        data->defaultAnimation = AnimationComponent::InvalidAnimationID;
-
-        for (auto& animVariant : animArgs.vars)
-        {
-            if (!animVariant.is<Nz::String>())
-            {
-                NazaraError("Animation codename: String expected");
-                continue;
-            }
-
-            Nz::String& animName = animVariant.get<Nz::String>();
-
-            if (!DoubleStores<AnimationData>::getInstance()->hasItem(animName))
-            {
-                NazaraError("Animation codename not found: " + animName);
-                continue;
-            }
-
-            AnimationData anim = DoubleStores<AnimationData>::getInstance()->getItem(animName); // Yup, copy ctor
-            data->animations.emplace_back(std::move(anim));
-        }
-
-        if (!data->animations.empty())
-            data->defaultAnimation = state.CheckField<std::size_t>("default", 0, -1);
-    }
-
-    state.Pop();
-
-    data->randomMovement = state.CheckField<CharacterData::RandomMovement>("random_movement", index);
-    data->attack = state.CheckField<CharacterData::Elements>("attack", index);
-    data->resistance = state.CheckField<CharacterData::Elements>("resistance", index);
-    data->fight = state.CheckField<CharacterData::Fight>("fight", index);
-
-
-    Nz::MaterialRef charMat = Nz::Material::New();
-    charMat->Configure("Translucent2D");
-    charMat->SetDiffuseMap(Nz::TextureLibrary::Get(state.CheckField<Nz::String>("texture", ":/game/unknown", index)));
-
-    auto size = state.CheckField<Nz::Vector2ui>("size", Nz::Vector2ui {}, index);
-
-    data->sprite = Nz::Sprite::New(charMat);
-    data->sprite->SetMaterial(charMat, false);
-    data->sprite->SetTextureRect({ 0u, 0u, size.x, size.y });
-    data->sprite->SetSize(float(size.x), float(size.y));
-
-    return 1;
-}
+inline unsigned int LuaImplQueryArg(const LuaState& state, int index, CharacterData::RandomMovement* random, TypeTag<CharacterData::RandomMovement>);
+inline unsigned int LuaImplQueryArg(const LuaState& state, int index, CharacterData::Fight* fight, TypeTag<CharacterData::Fight>);
+inline unsigned int LuaImplQueryArg(const LuaState& state, int index, CharacterData* data, TypeTag<CharacterData>);
 
 } // namespace Nz
+
+#include "characterdata.inl"
 
 #endif // CHARACTERDATA_HPP
