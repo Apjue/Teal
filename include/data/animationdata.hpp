cb0dd9ad982f9d94a67546d691f143fd9dba894b
S6066 <kiwimeteor6066@gmail.com>
2018-06-15 17:26:26 +0200

    Move AnimationData's utility functions & LuaImpl implementations to .inl

--- include/data/animationdata.hpp
+++ include/data/animationdata.hpp
@@ -20,38 +20,8 @@ struct AnimationData
         Emote
     };
 
-    static AnimationType stringToAnimationType(Nz::String string)
-    {
-        string = string.ToLower();
-
-        if (string == "walk")
-            return Walk;
-
-        if (string == "fight")
-            return Fight;
-
-        if (string == "emote")
-            return Emote;
-
-        throw std::runtime_error { "Undefined animation type!" };
-    }
-
-    static Nz::String animationTypeToString(AnimationType animType)
-    {
-        switch (animType)
-        {
-            case AnimationData::Walk:
-                return "walk";
-
-            case AnimationData::Fight:
-                return "fight";
-
-            case AnimationData::Emote:
-                return "emote";
-        }
-
-        throw std::runtime_error { "Undefined animation type!" };
-    }
+    static inline AnimationType stringToAnimationType(Nz::String string);
+    static inline Nz::String animationTypeToString(AnimationType animType);
 
     AnimationType type {};
     Nz::Vector2ui size {};
@@ -61,37 +31,15 @@ struct AnimationData
 };
 
 #include <Nazara/Lua/LuaState.hpp>
-#include <NDK/LuaAPI.hpp>
 
 namespace Nz
 {
 
-inline unsigned int LuaImplQueryArg(const LuaState& state, int index, AnimationData* animData, TypeTag<AnimationData>)
-{
-    state.CheckType(index, Nz::LuaType_Table);
-
-    animData->type = AnimationData::stringToAnimationType(state.CheckField<Nz::String>("type", index));
-    animData->size = state.CheckField<Nz::Vector2ui>("size", Nz::Vector2ui {}, index);
-    animData->texture = Nz::TextureLibrary::Get(state.CheckField<Nz::String>("texture", ":/game/unknown", index));
-    animData->offset = state.CheckField<Nz::Vector2f>("offset", Nz::Vector2f {}, index);
-    animData->frame = 0;
-
-    return 1;
-}
-
-/*inline int LuaImplReplyVal(const LuaState& state, AnimationData&& animData, TypeTag<AnimationData>)
-{
-    state.PushTable();
-    {
-        state.PushField("type", AnimationData::animationTypeToString(animData.type));
-        state.PushField("size", animData.size);
-        state.PushField("texture", animData.texture); <-- For this function to work, I'll need to retrieve texture ID (like ":/game/xxx") from texture
-        state.PushField("offset", animData.offset);
-    }
-
-    return 1;
-}*/
+inline unsigned int LuaImplQueryArg(const LuaState& state, int index, AnimationData* animData, TypeTag<AnimationData>);
+//inline int LuaImplReplyVal(const LuaState& state, AnimationData&& animData, TypeTag<AnimationData>);
 
 } // namespace Nz
+
+#include "animationdata.inl"
 
 #endif // ANIMATIONDATA_HPP
