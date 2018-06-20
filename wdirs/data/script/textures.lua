12b5537947872b0e9e4e980f7135803610af20e5
S6066 <kiwimeteor6066@gmail.com>
2018-06-20 13:29:03 +0200

    Add swords sprites

--- wdirs/data/script/textures.lua
+++ wdirs/data/script/textures.lua
@@ -1,12 +1,23 @@
 teal_textures =
 {
+    { ":/game/unknown",       "game/main/unknown.png" },
     { ":/game/money",         "game/main/money.png" },
     { ":/game/scheme",        "game/main/scheme.png" },
     { ":/game/teal",          "game/main/teal.png" },
     { ":/game/tileset",       "game/main/tileset.png" },
     { ":/game/fight_tileset", "game/main/tileset_fight.png" },
     { ":/game/char/villager", "game/char/villager.png" },
-    { ":/game/unknown",       "game/main/unknown.png" },
+
+    { ":/game/items/swords/broadsword_cold", "game/items/swords/broadsword_cold.png" },
+    { ":/game/items/swords/broadsword_fire", "game/items/swords/broadsword_fire.png" },
+    { ":/game/items/swords/broadsword_holy", "game/items/swords/broadsword_holy.png" },
+    { ":/game/items/swords/broadsword_mithril", "game/items/swords/broadsword_mithril.png" },
+    { ":/game/items/swords/broadsword_steel", "game/items/swords/broadsword_steel.png" },
+    { ":/game/items/swords/longsword_cold", "game/items/swords/longsword_cold.png" },
+    { ":/game/items/swords/longsword_copper", "game/items/swords/longsword_copper.png" },
+    { ":/game/items/swords/longsword_flame", "game/items/swords/longsword_flame.png" },
+    { ":/game/items/swords/longsword_holy", "game/items/swords/longsword_holy.png" },
+    { ":/game/items/swords/longsword_steel", "game/items/swords/longsword_steel.png" },
 
     { ":/buttons/equip",         "buttons/bar/equip.png" },
     { ":/buttons/equip_pressed", "buttons/bar/equip_pressed.png" },
