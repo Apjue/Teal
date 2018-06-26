78bcc3c2a5ca4a318d0a9978c3150074e068f143
S6066 <kiwimeteor6066@gmail.com>
2018-06-26 18:16:53 +0200

    Move shields textures to the correct folder

--- wdirs/data/script/textures.lua
+++ wdirs/data/script/textures.lua
@@ -6,18 +6,23 @@ teal_textures =
     { ":/game/teal",          "game/main/teal.png" },
     { ":/game/tileset",       "game/main/tileset.png" },
     { ":/game/fight_tileset", "game/main/tileset_fight.png" },
+
     { ":/game/char/villager", "game/char/villager.png" },
 
-    { ":/game/items/swords/broadsword_cold", "game/items/swords/broadsword_cold.png" },
-    { ":/game/items/swords/broadsword_fire", "game/items/swords/broadsword_fire.png" },
-    { ":/game/items/swords/broadsword_holy", "game/items/swords/broadsword_holy.png" },
+    { ":/game/items/swords/broadsword_cold",    "game/items/swords/broadsword_cold.png" },
+    { ":/game/items/swords/broadsword_fire",    "game/items/swords/broadsword_fire.png" },
+    { ":/game/items/swords/broadsword_holy",    "game/items/swords/broadsword_holy.png" },
     { ":/game/items/swords/broadsword_mithril", "game/items/swords/broadsword_mithril.png" },
-    { ":/game/items/swords/broadsword_steel", "game/items/swords/broadsword_steel.png" },
-    { ":/game/items/swords/longsword_cold", "game/items/swords/longsword_cold.png" },
-    { ":/game/items/swords/longsword_copper", "game/items/swords/longsword_copper.png" },
-    { ":/game/items/swords/longsword_flame", "game/items/swords/longsword_flame.png" },
-    { ":/game/items/swords/longsword_holy", "game/items/swords/longsword_holy.png" },
-    { ":/game/items/swords/longsword_steel", "game/items/swords/longsword_steel.png" },
+    { ":/game/items/swords/broadsword_steel",   "game/items/swords/broadsword_steel.png" },
+    { ":/game/items/swords/longsword_cold",     "game/items/swords/longsword_cold.png" },
+    { ":/game/items/swords/longsword_copper",   "game/items/swords/longsword_copper.png" },
+    { ":/game/items/swords/longsword_flame",    "game/items/swords/longsword_flame.png" },
+    { ":/game/items/swords/longsword_holy",     "game/items/swords/longsword_holy.png" },
+    { ":/game/items/swords/longsword_steel",    "game/items/swords/longsword_steel.png" },
+
+    { ":/game/items/shields/shield_copper", "game/items/shields/shield_copper.png" },
+    { ":/game/items/shields/shield_gold",   "game/items/shields/shield_gold.png" },
+
 
     { ":/buttons/equip",         "buttons/bar/equip.png" },
     { ":/buttons/equip_pressed", "buttons/bar/equip_pressed.png" },
