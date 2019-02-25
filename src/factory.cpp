108f853e2095831739a77d9a66a15cb6c58a642f
S6066 <kiwimeteor6066@gmail.com>
2019-02-25 18:11:00 +0100

    It's 2019 by the way

--- src/factory.cpp
+++ src/factory.cpp
@@ -1,4 +1,4 @@
-// Copyright (C) 2018 Samy Bensaid
+// Copyright (C) 2019 Samy Bensaid
 // This file is part of the Teal project.
 // For conditions of distribution and use, see copyright notice in LICENSE
 
